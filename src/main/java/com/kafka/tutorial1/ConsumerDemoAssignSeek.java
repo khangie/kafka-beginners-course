df4f420dd09bee45056fa3cfff8f8199cc734288
Khang Nguyen <khangie@hotmail.com>
2020-01-11 08:37:09 -0800

    added seekers

---
+++ src/main/java/com/kafka/tutorial1/ConsumerDemoAssignSeek.java
@@ -0,0 +1,67 @@
+package com.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoAssignSeek {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String topic = "first_topic";
+
+        // Create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Read from the very beginning of the topic
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // Assign and seek are mostly used to replay data or fetch a specific message
+
+        // Assign
+        TopicPartition partitionToReadFrom = new TopicPartition(topic, 0);
+        consumer.assign(Arrays.asList(partitionToReadFrom));
+
+        // Seek
+        long offsetToReadFrom = 15L;
+        consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        int numberOfMessagesReadSoFar = 0;
+        boolean keepOnReading = true;
+
+        // Poll for new data
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord record : records) {
+                numberOfMessagesReadSoFar += 1;
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                if (numberOfMessagesReadSoFar >= numberOfMessagesToRead) {
+                    keepOnReading = false;
+                    logger.info("Exiting the application");
+                    break;
+                }
+            }
+        }
+    }
+}
