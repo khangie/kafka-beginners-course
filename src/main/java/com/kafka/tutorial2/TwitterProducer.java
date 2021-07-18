04305e46d575654cbc09581548a06d85f1aeea80
Khang Nguyen <khangie@hotmail.com>
2021-07-18 07:52:06 -0700

    twitter producer

---
+++ src/main/java/com/kafka/tutorial2/TwitterProducer.java
@@ -0,0 +1,59 @@
+package com.kafka.tutorial2;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TwitterProducer {
+
+    public TwitterProducer() {
+
+    }
+
+    public static void main(String[] args) {
+
+        new TwitterProducer().run();
+
+    }
+
+    public void run() {
+
+        // create a twitter client
+
+        // create a kafka producer
+
+        // loop to send tweets to kafka
+
+    }
+
+    String consumerKey = "";
+    String consumerSecret = "";
+    String token = "";
+    String secret = "";
+
+    public void createTwitterClient() {
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+        // Optional: set up some followings and track terms
+        List<String> terms = Lists.newArrayList("kafka");
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+
+    }
+
+}
